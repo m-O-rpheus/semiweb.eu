4c2329c34810ece0edc3d0b9b89e93bfb32e09ba
m0rph3u <73857179+m0rph3u@users.noreply.github.com>
2022-02-02 17:12:11 +0100

    Update example Click eventListener (Simple).js

--- example Click eventListener (Simple).js
+++ example Click eventListener (Simple).js
@@ -1,13 +1,10 @@
-    
-    
-    swapinode().addEventListener('click', function(e) {
+// Registriert ein Click Event auf das Element selbst, oder ein Kind davon
+swapinode().addEventListener('click', function(e) {
 
-        let sel = '.EXAMPLE_CLASS'; sel = ( ( e.target.matches(sel) === true ) ? e.target : ( e.target.closest(sel) !== null ) ? e.target.closest(sel) : null );
+    let sel = '.EXAMPLE_CLASS'; sel = ( ( e.target.matches(sel) === true ) ? e.target : ( e.target.closest(sel) !== null ) ? e.target.closest(sel) : null );
 
-        if( sel !== null ) {
+    if( sel !== null ) {
 
-            console.log(sel.dataset.example);
-        }
-    });
-    
-    
+        console.log(sel.dataset.example);
+    }
+});
